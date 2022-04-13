8fa335ce81a26c76d5de8e1a77a0780b61877311
captainAyan <ayanchakraborty105@gmail.com>
2022-04-13 20:21:41 +0530

    added reset, winning and losing system

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# minesweeper
+<p align="center">
+  <h1 align="center">MineSweeper 🚩</h1>
+  <p align="center">
+    A simple Minesweeper implementation using Python and TKinter (Not PyGame)
+  </p>
+</p>
+<p align="center">
+<a href="https://github.com/captainAyan/minesweeper"><img src="https://raw.githubusercontent.com/captainAyan/minesweeper/main/image.png"></a>
+</p>
+
+## Getting Started
+- Clone the repo
+- run the `main.py`
+
+🙏🥺 Consider starring this project.
+
+👉👈 Also consider contribution, I could use some help.
+
+## Contact
+Send me a message on discord or instagram. Check out my [Profile Readme](https://github.com/captainAyan)
--- main.py
+++ main.py
@@ -22,9 +22,39 @@ class MainApplication(tk.Frame):
         board.place(x=0, y=60)
 
         custom_font = tkFont.Font(size=16, weight="normal", family="Arial")
-        btn_style = {'borderwidth': "0", 'font': custom_font, 'bd': '2', 'fg': "black", 'anchor': "w", 'padx': 5, 'bg': '#dedede'}
+        btn_style = {'borderwidth': "0", 'font': custom_font, 'bd': '2', 'fg': "black", 'anchor': "w", 'padx': 5,
+                     'bg': '#dedede'}
         btn_size = {'height': 30, 'width': 30}
 
+        reset_btn = tk.Button(self.parent, text="Reset", command=lambda: self.reset())
+        reset_btn.place(height=20, width=50, x=10, y=10)
+
+        self.game_state_string = StringVar()
+        self.game_state_string.set("Playing")
+        game_state_label = tk.Label(self.parent, textvariable=self.game_state_string)
+        game_state_label.place(x=70, y=10)
+
+        self.board = None
+        self.bombs = None
+        self.buttons = None
+        self.total_flag = 0
+
+        self.setup()
+
+        # SETUP BUTTONS
+        self.buttons = []
+        for x in range(10):
+            column = []
+            for y in range(10):
+                # SETTING UP BUTTONS
+                btn = tk.Button(board, command=lambda i=x, j=y: self.check(i, j), **btn_style)
+                btn.bind("<Button-2>", lambda event, i=x, j=y: self.flag(i, j))  # right click
+                btn.bind("<Button-3>", lambda event, i=x, j=y: self.flag(i, j))  # right click
+                btn.place(**btn_size, x=x * 30, y=y * 30)
+                column.append(btn)
+            self.buttons.append(column)
+
+    def setup(self):
         # BOARD ARRAY
         self.board = []
         for x in range(10):
@@ -40,25 +70,14 @@ class MainApplication(tk.Frame):
             x = i % 10
             self.board[x][y].value = "x"
 
-        # SETUP BUTTONS
-        self.buttons = []
+        # SETUP CELL VALUES
         for x in range(10):
-            column = []
             for y in range(10):
                 total_bombs = 0
-
-                # SETTING UP BUTTONS
-                btn = tk.Button(board, command=lambda i=x, j=y: self.check(i, j), **btn_style)
-                btn.bind("<Button-2>", lambda event, i=x, j=y: self.flag(i, j))  # right click
-                btn.bind("<Button-3>", lambda event, i=x, j=y: self.flag(i, j))  # right click
-                btn.place(**btn_size, x=x * 30, y=y * 30)
-                column.append(btn)
-
                 if self.board[x][y].value == "x":
                     continue
 
                 # SETTING UP NUMBERS TO CELLS
-
                 # TOP
                 # |x| | |
                 # | |o| |
@@ -110,7 +129,6 @@ class MainApplication(tk.Frame):
                     total_bombs += 1
 
                 self.board[x][y].value = str(total_bombs)
-            self.buttons.append(column)
 
     def check(self, x, y):
         if self.game_state == 'playing' and not self.board[x][y].flagged and not self.board[x][y].checked:
@@ -141,7 +159,8 @@ class MainApplication(tk.Frame):
                 # |x| | |
                 # | |o| |
                 # | | | |
-                if x - 1 >= 0 and y - 1 >= 0 and not self.board[x - 1][y - 1].checked and self.board[x - 1][y - 1].value != "x":
+                if x - 1 >= 0 and y - 1 >= 0 and not self.board[x - 1][y - 1].checked and self.board[x - 1][
+                    y - 1].value != "x":
                     self.check(x - 1, y - 1)
 
                 # | |x| |
@@ -153,7 +172,8 @@ class MainApplication(tk.Frame):
                 # | | |x|
                 # | |o| |
                 # | | | |
-                if x + 1 < 10 and y - 1 >= 0 and not self.board[x + 1][y - 1].checked and self.board[x + 1][y - 1].value != "x":
+                if x + 1 < 10 and y - 1 >= 0 and not self.board[x + 1][y - 1].checked and self.board[x + 1][
+                    y - 1].value != "x":
                     self.check(x + 1, y - 1)
 
                 # SIDE
@@ -173,7 +193,8 @@ class MainApplication(tk.Frame):
                 # | | | |
                 # | |o| |
                 # |x| | |
-                if x - 1 >= 0 and y + 1 < 10 and not self.board[x - 1][y + 1].checked and self.board[x - 1][y + 1].value != "x":
+                if x - 1 >= 0 and y + 1 < 10 and not self.board[x - 1][y + 1].checked and self.board[x - 1][
+                    y + 1].value != "x":
                     self.check(x - 1, y + 1)
 
                 # | | | |
@@ -185,7 +206,8 @@ class MainApplication(tk.Frame):
                 # | | | |
                 # | |o| |
                 # | | |x|
-                if x + 1 < 10 and y + 1 < 10 and not self.board[x + 1][y + 1].checked and self.board[x + 1][y + 1].value != "x":
+                if x + 1 < 10 and y + 1 < 10 and not self.board[x + 1][y + 1].checked and self.board[x + 1][
+                    y + 1].value != "x":
                     self.check(x + 1, y + 1)
         pass
 
@@ -195,17 +217,28 @@ class MainApplication(tk.Frame):
                 if self.board[x][y].flagged:
                     self.board[x][y].flagged = False
                     self.buttons[x][y]['background'] = '#dedede'
+                    self.total_flag -= 1
                 else:
-                    self.board[x][y].flagged = True
-                    self.buttons[x][y]['background'] = '#ff0000'
+                    if self.total_flag < 10: # only 10 flags are allowed
+                        self.board[x][y].flagged = True
+                        self.buttons[x][y]['background'] = '#ff0000'
+                        self.total_flag += 1
+
+                        for i in self.bombs:
+                            y = math.floor(i / 10)
+                            x = i % 10
+                            if not self.board[x][y].flagged:
+                                return
+                        self.game_state = 'won'
+                        self.endgame()
         pass
 
     def endgame(self):
         if self.game_state == 'lost':
             self.reveal_bombs()
-            print('lost')
+            self.game_state_string.set("You Lost")
         elif self.game_state == 'won':
-            print('won')
+            self.game_state_string.set("You Won")
         pass
 
     def reveal_bombs(self):
@@ -217,6 +250,21 @@ class MainApplication(tk.Frame):
             self.buttons[x][y]['text'] = self.board[x][y].value
             self.buttons[x][y]['disabledforeground'] = '#ffa500'
         pass
+
+    def reset(self):
+        self.board = None
+        self.bombs = None
+        self.total_flag = 0
+
+        for x in range(10):
+            for y in range(10):
+                self.buttons[x][y]['background'] = '#dedede'
+                self.buttons[x][y]['state'] = 'normal'
+                self.buttons[x][y]['text'] = ''
+
+        self.game_state = 'playing'
+        self.game_state_string.set("Playing")
+        self.setup()
 
 
 if __name__ == "__main__":
